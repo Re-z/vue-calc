32c8a8cdd6b2de4eb39ee7f6c91f680a63b63a8c
Re-z <re-z@ukr.net>
2020-01-08 01:24:03 +0200

    done, log left

---
+++ README.md
@@ -0,0 +1,7 @@
+# Vue.js calculator
+## Demo: https://re-z.github.io/vue-calc/index.html
+## Techstack:
+- HTML
+- SASS + Css Grid
+- Vue.js
+- Gulp.js
--- js/app.js
+++ js/app.js
@@ -1,18 +1,32 @@
 const v = new Vue({
 	el: '.wrap',
 	data: {
-		output: ''
+		output: '0'
 	},
 	methods: {
 		addToExpression(ev) {
 			const val = ev.target.innerHTML;
-			this.output += val;
-		},
-		clear() {
-			this.output = ''
+			//запрещаем ставить первым символом некоторые операторы
+			if(this.output.length === 0 && (val === '%' || val === '*'|| val === '/')) {
+				return
+			} 
+			else if(this.output === '0' && val !== '.') {
+				this.output = val
+			}
+			else {
+				this.output += val;
+			}
 		},
 		handleExpression() {
-			this.output = Number(this.output)
+			//eval - "выполняет" строку
+			this.output = eval(this.output)
+		},
+		clear() {
+			this.output = '0'
+		},
+		trim() {
+			const trimmedStr = String(this.output).slice(0,-1)
+			this.output = trimmedStr
 		}
 	}
 })
